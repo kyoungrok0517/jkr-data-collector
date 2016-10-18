3bfc4ff533c2a88fa4768011167d65d7e94f7735
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-10-18 21:53:24 +0900

    build project structure

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r', encoding='utf-8') as f:
+        return f.read()
 
 setup(
     name='jkr-data-collector',
     version='0.0.1',
-    packages=['twitter'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     url='https://github.com/kyoungrok0517/jkr-data-collector',
     license='',
+    description='KJang\'s personal data collector',
+    long_description=readme(),
     author='Kyoung-Rok Jang',
     author_email='kyoungrok.jang@gmail.com',
-    description='My personal data collector'
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'plac'
+    ],
+    zip_safe=False
 )
