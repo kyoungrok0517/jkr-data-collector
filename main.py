e71d2d3b19e01de9a7aeaa4d2b6bedf5787da1ab
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-10-19 11:38:00 +0900

    switch to click for cmd interface

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 
 
 """
-import plac
+from __future__ import print_function
+from __future__ import with_statement
+import click
 import logging
-import os
-import json
-from twitter.list import get_list_tweets
+import twitter_collector
+
 
 # set logging
 LOG_FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
@@ -15,41 +16,19 @@ LOG_LEVEL = 'INFO'
 logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL)
 logger = logging.getLogger(__file__)
 
+# available commands
+sources = 'twitter news'.split()
 
-@plac.annotations(
-    target_lists=plac.Annotation('the target lists (every list if not specified)', 'option', 'l'),
-    limit=plac.Annotation('The number of tweets to get from each target list', 'option', 'n', type=int),
-    output_file=plac.Annotation('The output file path', 'option', 'o')
-)
-def twitter_list(target_lists=None, limit=10, output_file=None):
-    """Get the tweets from lists
 
-    Arguments:
-        target_lists (list): the target lists (every list if not specified)
-        limit (int): the number of tweets to get from each target list
-        output_file (str): the output file path
-    """
-    if not target_lists:
-        target_lists = []
+def main(*target_sources):
+    if target_sources:
+        # twitter
+        if 'twitter' in target_sources:
+            pass
 
-    if output_file:
-        if not os.path.exists(output_file):
-            # prepare directories
-            dirs = os.path.dirname(output_file)
-            if not dirs == '.':
-                os.makedirs(dirs)
-
-            # output to file
-            with open(output_file, 'w', encoding='utf-8') as fout:
-                for tw in get_list_tweets(limit, target_lists):
-                    fout.write(json.dumps(tw) + '\n')
-        else:
-            raise OSError('The output file already exists! Quitting the program...')
-    else:
-        for tw in get_list_tweets(limit, target_lists):
-            logger.info(tw)
+        # news
+        if 'news' in target_sources:
+            pass
 
 if __name__ == '__main__':
-    # twitter list
-    plac.call(twitter_list)
-
+    pass
